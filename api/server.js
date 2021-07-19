3f629ca9c7925fc67f5ae28c643d0d50bd9079a2
davedwight <dpdwight@gmail.com>
2021-07-19 17:18:18 -0600

    auth router connected to server

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const usersRouter = require('./users/users-router');
 
+const usersRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -27,6 +28,7 @@ server.get("/", (req, res) => {
 });
 
 server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
