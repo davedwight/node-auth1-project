49647ed4da686348c74ee58e45ae7d27a094fa41
davedwight <dpdwight@gmail.com>
2021-07-19 16:59:15 -0600

    server, router, model connected and find working

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,15 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+const router = require("express").Router();
+const User = require("../users/users-model");
 
-
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err)
+  }
+});
 /**
   [GET] /api/users
 
@@ -24,5 +33,5 @@
   }
  */
 
-
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
